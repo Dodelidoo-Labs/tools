6cb7b813f4c73b639df50cf3fe1bf87b75706ae7
Beda Schmid <beda@tukutoi.com>
2023-06-16 12:01:40 +0700

    Update ARCleaner
    - Cleaner more smart
    - Stats include path to images

--- arcleaner/archecker.py
+++ arcleaner/archecker.py
@@ -3,6 +3,7 @@ import csv
 from PIL import Image
 import glob
 from fractions import Fraction
+import shutil
 
 Image.MAX_IMAGE_PIXELS = 100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
 def get_image_dimensions(image_path):
@@ -21,16 +22,33 @@ def analyze_image_dimensions(folder_path):
             aspect_ratio_str = f"{aspect_ratio.numerator}:{aspect_ratio.denominator}"
             image_dimensions.setdefault(dimensions, {"aspect_ratio": aspect_ratio_str, "count": 0})
             image_dimensions[dimensions]["count"] += 1
+
+            # Copy and resize the image to a 150x150 size
+            image_name = os.path.basename(image_path)
+            new_image_path = os.path.join(folder_path, "resized_images", image_name)
+            resize_image(image_path, new_image_path, (150, 150))
+
+            # Add the image path to the image_dimensions dictionary
+            image_dimensions[dimensions].setdefault("image_paths", [])
+            image_dimensions[dimensions]["image_paths"].append(new_image_path)
+
         except (IOError, OSError, ValueError):
             print(f"Failed to process image: {image_path}")
     return image_dimensions
 
+def resize_image(image_path, new_image_path, size):
+    with Image.open(image_path) as image:
+        image.thumbnail(size)
+        image.save(new_image_path)
+
+
 def write_image_dimensions_to_csv(csv_file_path, image_dimensions):
     with open(csv_file_path, "w", newline="") as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(["Image Dimensions", "Aspect Ratio", "Image Count"])  # Write the header row
+        writer.writerow(["Image Dimensions", "Aspect Ratio", "Image Count", "Image", "Image Path"])  # Write the header row
         for dimensions, data in image_dimensions.items():
-            writer.writerow([dimensions, data["aspect_ratio"], data["count"]])  # Write the data rows
+            for image_path in data.get("image_paths", []):
+                writer.writerow([dimensions, data["aspect_ratio"], data["count"], image_path, os.path.abspath(image_path)])  # Write the data rows
 
 # Ask the user for the directory path
 directory_path = input("Enter the directory path: ")
@@ -43,10 +61,15 @@ if not os.path.isdir(directory_path):
     print("Invalid directory path.")
     exit()
 
+
+# Create the "resized_images" directory
+resized_images_dir = os.path.join(directory_path, "resized_images")
+os.makedirs(resized_images_dir, exist_ok=True)
+
 # Analyze the dimensions and aspect ratios of images in the directory and its subdirectories recursively
 image_dimensions = analyze_image_dimensions(directory_path)
 
 # Write the image dimensions, aspect ratios, and image counts to the CSV file
 write_image_dimensions_to_csv(csv_file_path, image_dimensions)
 
-print(f"Image dimensions, aspect ratios, and counts have been written to '{csv_file_path}'.")
+print(f"Image dimensions, aspect ratios, counts, image paths, and resized images have been written to '{csv_file_path}'.")
--- arcleaner/arcleaner.py
+++ arcleaner/arcleaner.py
@@ -1,65 +1,62 @@
 import os
-from collections import Counter
 from PIL import Image
-from pathlib import Path
-
 Image.MAX_IMAGE_PIXELS = 100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
 
-def aspect_ratio(width, height):
-    ratio = width / height
-    return round(ratio, 2)
+aspect_ratios = [(16, 9), (1, 1), (8, 5), (3, 2), (4, 3), (3, 4), (2, 3), (2048, 1365), (5, 4), (43, 18)]
 
-def closest_aspect_ratio(width, height, aspect_ratios):
-    ratio = width / height
-    closest_ratio = min(aspect_ratios, key=lambda x: abs(x - ratio))
-    return closest_ratio
+def get_closest_aspect_ratio(file_path, width, height):
+    min_diff = float("inf")
+    closest_width, closest_height = width, height
 
-def resize_or_crop_image(image_path, aspect_ratios):
-    img = Image.open(image_path)
-    width, height = img.size
+    for ratio in aspect_ratios:
+        for w in range(width-50, width+50):
+            h = w * ratio[1] / ratio[0]
+            diff = abs(w - width) + abs(h - height)
 
-    # Skip images that are smaller than 1200x900 or 900x1200
-    if width < 1200 and height < 900 or width < 900 and height < 1200:
-        print(f"Skipping {image_path} because it is too small")
-        return
+            if diff < min_diff:
+                min_diff = diff
+                closest_width, closest_height = w, int(h)
 
-    aspect_ratio = closest_aspect_ratio(width, height, aspect_ratios)
+    # Add user input check after each image processing
+    # if abs(closest_width - width) > 500 or abs(closest_height - height) > 500:
+    #     proceed = input(f"the image {file_path} would become {closest_width} x {closest_height} from {width} x {height}. Proceed? y/n:")
+    #     if proceed.lower() != 'y':
+    #         print(f"the image {file_path} will stay the same {closest_width} x {closest_height} from {width} x {height}")
+    #         return  width, height
 
-    if abs(aspect_ratio - width/height) < 0.01:  # Adjust this value to control how close the aspect ratios need to be
-        new_height = round(width / aspect_ratio)
-        img = img.resize((width, new_height), Image.ANTIALIAS)
-    else:  # If resizing isn't possible, crop
-        if aspect_ratio > width/height:
-            new_height = round(width / aspect_ratio)
-            img = img.crop((0, 0, width, new_height))
-        else:
-            new_width = round(height * aspect_ratio)
-            img = img.crop((0, 0, new_width, height))
+    #print(f"the image {file_path} will become {closest_width} x {closest_height} from {width} x {height}")
+    return closest_width, closest_height
 
-    img.save(image_path)
+def process_image(file_path):
+    with Image.open(file_path) as img:
+        width, height = img.size
+        original_aspect_ratio = (width, height)
 
-def main():
-    path = input("Enter the path to the directory: ")
-    aspect_ratios_counter = Counter()
+        if original_aspect_ratio not in aspect_ratios:
+            resize_width, resize_height = get_closest_aspect_ratio(file_path, width, height)
 
-    # First pass: find the aspect ratios and count the occurrences
-    for filename in Path(path).rglob('*'):
-        if filename.is_file() and filename.suffix.lower() in ['.png', '.jpg', '.jpeg']:
-            img = Image.open(filename)
-            width, height = img.size
-            aspect_ratios_counter[aspect_ratio(width, height)] += 1
+            # Check if resize is possible
+            if abs(resize_width - width) <= 50 and abs(resize_height - height) <= 50:
+                resized_img = img.resize((resize_width, resize_height))
+                resized_img.save(file_path)
+                print(f'Resized: {file_path}')
+            else:
+                # Crop image
+                left = (width - resize_width)/2
+                top = (height - resize_height)/2
+                right = (width + resize_width)/2
+                bottom = (height + resize_height)/2
 
-    # Get aspect ratios with fewer than 10 images or not a multiple of 10 images
-    filtered_ratios = [ratio for ratio, count in aspect_ratios_counter.items() if count < 10 or count % 10 != 0]
+                cropped_img = img.crop((left, top, right, bottom))
+                cropped_img.save(file_path)
+                print(f'Cropped: {file_path}')
 
-    # Filter out aspect ratios that have at least 10 images or a multiple of 10 images
-    aspect_ratios = [ratio for ratio in aspect_ratios_counter.keys() if ratio not in filtered_ratios]
+def scan_folder(folder_path):
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            if file.endswith('.png') or file.endswith('.jpg'):
+                file_path = os.path.join(root, file)
+                process_image(file_path)
 
-    # Second pass: resize or crop the images
-    for filename in Path(path).rglob('*'):
-        if filename.is_file() and filename.suffix.lower() in ['.png', '.jpg', '.jpeg']:
-            print(f"Processing {filename}")
-            resize_or_crop_image(str(filename), aspect_ratios)
-
-if __name__ == "__main__":
-    main()
+custom_path = input("Enter the Folder path: ")
+scan_folder(custom_path)
